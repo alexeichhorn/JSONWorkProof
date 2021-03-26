6f0eb98e66b9fa179a8d715c936575889dbd4805
Alexander Eichhorn <a.eichhorn@losjet.com>
2021-03-26 02:06:22 +0100

    added measure tests

--- Tests/JSONWorkProofTests/JSONWorkProofTests.swift
+++ Tests/JSONWorkProofTests/JSONWorkProofTests.swift
@@ -2,13 +2,30 @@ import XCTest
 @testable import JSONWorkProof
 
 final class JSONWorkProofTests: XCTestCase {
+    
     func testExample() {
         let jwp = JWP()
         let token = try! jwp.generate(claims: ["hello": "world"])
         print(token)
     }
+    
+    func testMintSHA256DefaultSpeed() {
+        let jwp = JWP()
+        measure {
+            _ = try! jwp.generate(claims: ["test": "speedtest"])
+        }
+    }
+    
+    func testMintSHA256FastSpeed() {
+        let jwp = JWP(difficulty: 15)
+        measure {
+            _ = try! jwp.generate(claims: ["test": "speedtest"])
+        }
+    }
 
     static var allTests = [
         ("testExample", testExample),
+        ("testMintSHA256DefaultSpeed", testMintSHA256DefaultSpeed),
+        ("testMintSHA256FastSpeed", testMintSHA256FastSpeed)
     ]
 }
